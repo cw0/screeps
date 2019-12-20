95c9bd3d2d2532f9698851628aa6304ad59e0c76
chris-PC\chris <cwilson8706@gmail.com>
2019-12-19 22:58:25 -0500

    add eslint rules

--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ const loop = () => {
   console.log('test');
 };
 
+const handleCreep = (creep) => {
+  if (creep.memory.working === true) {
+    //TODO find closest spawn
+    if (creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+      creep.moveTo(Game.spawns.Spawn1);
+    }
+  }
+  else {
+    creep.pos.findClosestByPath(FIND_SOURCES);
+  }
+};
+
 module.exports = {
   loop,
 };
