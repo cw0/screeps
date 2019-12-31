1ab618d2bb366bb91aeac25de66f9918beb6f119
cw0 <element4emerica@gmail.com>
2019-12-31 14:20:57 -0500

    fix typings

--- src/main.js
+++ src/main.js
@@ -1,18 +1,30 @@
-const loop = () => {
-  console.log('test');
-};
-
 const handleCreep = (creep) => {
+  if (creep.memory.working === true && creep.carry.energy === 0) {
+    creep.memory.working = false;
+  } else if (creep.memory.working === false && creep.carry.energy === creep.carry.carryCapacity) {
+    creep.memory.working = true;
+  }
+
   if (creep.memory.working === true) {
     //TODO find closest spawn
     if (creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
       creep.moveTo(Game.spawns.Spawn1);
     }
-  }
-  else {
-    creep.pos.findClosestByPath(FIND_SOURCES);
+  } else {
+    const source = creep.pos.findClosestByPath(FIND_SOURCES);
+    if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
+      creep.moveTo(source);
+    }
   }
 };
+
+const loop = () => {
+  // console.log('test');
+  Object.values(Game.creeps).forEach((creep) => {
+    handleCreep(creep);
+  });
+};
+
 
 module.exports = {
   loop,
